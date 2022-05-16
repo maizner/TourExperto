33a31cfb1a399c951682206ad4db192c93c501a5
Maia Aizner <info@maiaaizner.com.ar>
2022-05-16 15:42:13 -0300

    my reervations

---
+++ dist/content/js/my-reservations.js
@@ -0,0 +1,25 @@
+var myreservations = {
+    
+    init: function () {
+        
+     
+    }
+       
+    
+};
+
+
+// -----------------------------------------------------------------------------
+//FUNCIONES
+// ----------------------------------------------------------------------------- 
+
+
+
+// -----------------------------------------------------------------------------
+//FIN FUNCIONES
+// ----------------------------------------------------------------------------- 
+
+$(function () {
+    myreservations.init();
+});
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJteS1yZXNlcnZhdGlvbnMuanMiXSwic291cmNlc0NvbnRlbnQiOlsidmFyIG15cmVzZXJ2YXRpb25zID0ge1xyXG4gICAgXHJcbiAgICBpbml0OiBmdW5jdGlvbiAoKSB7XHJcbiAgICAgICAgXHJcbiAgICAgXHJcbiAgICB9XHJcbiAgICAgICBcclxuICAgIFxyXG59O1xyXG5cclxuXHJcbi8vIC0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tXHJcbi8vRlVOQ0lPTkVTXHJcbi8vIC0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tIFxyXG5cclxuXHJcblxyXG4vLyAtLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLVxyXG4vL0ZJTiBGVU5DSU9ORVNcclxuLy8gLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0gXHJcblxyXG4kKGZ1bmN0aW9uICgpIHtcclxuICAgIG15cmVzZXJ2YXRpb25zLmluaXQoKTtcclxufSk7Il0sImZpbGUiOiJteS1yZXNlcnZhdGlvbnMuanMifQ==
